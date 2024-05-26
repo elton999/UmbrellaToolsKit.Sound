f439efcbd2e2a3d06698e27e2dd31a141c717075
Elton Silva <eltoncarsilva73@gmail.com>
2024-05-26 17:19:06 -0300

    Fmod integration

---
+++ SoundManager.cs
@@ -0,0 +1,64 @@
+﻿using FMOD.Studio;
+using System;
+
+namespace UmbrellaToolsKit.Sound
+{
+    public class SoundManager
+    {
+        public FMOD.Studio.System SoundSystem;
+        public FMOD.Studio.Bank LocalizedBank;
+
+        public SoundManager Instance { get; }
+
+        private SoundManager _instance;
+
+        public SoundManager(string bankPath) 
+        {
+            if (_instance != null) return;
+
+            _instance = this;
+
+            var result = FMOD.Studio.System.create(out SoundSystem);
+
+            if (!LogResult(result)) return;
+
+            result = SoundSystem.initialize(512, FMOD.Studio.INITFLAGS.NORMAL, FMOD.INITFLAGS.NORMAL, new IntPtr(0));
+
+            if (!LogResult(result)) return;
+
+            result = SoundSystem.loadBankFile(System.IO.Path.GetFullPath(bankPath), FMOD.Studio.LOAD_BANK_FLAGS.NORMAL, out LocalizedBank);
+
+            if (!LogResult(result)) return;
+        }
+
+        public void Update() => SoundSystem.update();
+
+        public void Dispose()
+        {
+            SoundSystem.release();
+            LocalizedBank.unload();
+        }
+
+        public EventInstance GetEventInstance(FMOD.GUID id)
+        {
+            EventDescription eventDescription;
+            EventInstance eventInstance;
+
+            if(!LogResult(SoundSystem.getEventByID(id, out eventDescription))) return default;
+
+            if(!LogResult(eventDescription.createInstance(out eventInstance))) return default;
+
+            return eventInstance;
+        }
+
+        public static bool LogResult(FMOD.RESULT result)
+        {
+            if (result != FMOD.RESULT.OK)
+            {
+                Console.WriteLine($"FMOD error! {result} {FMOD.Error.String(result)}\n");
+                return false;
+            }
+            return true;
+        }
+    }
+}
